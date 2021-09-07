2dec139b08be063583b65fc705f1aaa7373f303f
macds <macds@macds.local>
2021-09-07 15:17:17 +0300

    find and Eq

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,20 @@
+///  <reference types="Cypress" /> 
+
+
+// describe("Popolnenie mobolnogo telefona", () => {
+//     context("Popolnenie minimalnoy summu", () => {
+//         it("Pokazuvaet owibky: Minimum popolneniya na 1 UAH", () => {
+
+
+
+//         })
+
+//     })
+
+// })
+
+it("By id", () => {
+    cy.vieport(1800, 700)
+    cy.visit('https://next.privat24.ua/deposit')
+    cy.get('tbody>:nth-child(1)>:nth-child(8)')
+})
